48981cb94a0b8e2a468e462f915860906aa61221
jonte200488 <107001212+jonte200488@users.noreply.github.com>
2024-08-11 06:56:07 +0000

    1

--- pages/api/players.js
+++ pages/api/players.js
@@ -1,24 +1,33 @@
-// pages/api/players.js
+// /pages/api/players.js
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    const players = await prisma.player.findMany();
-    res.status(200).json(players);
-  } else if (req.method === 'POST') {
-    const { name, image } = req.body;
-    const newPlayer = await prisma.player.create({
-      data: {
-        name,
-        image,
-        points: 0,
-      },
-    });
-    res.status(201).json(newPlayer);
-  } else {
-    res.setHeader('Allow', ['GET', 'POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+  try {
+    if (req.method === 'GET') {
+      const players = await prisma.player.findMany();
+      if (players.length === 0) {
+        // Handle the case where no players are found
+        return res.status(200).json({ message: 'No players found', players: [] });
+      }
+      res.status(200).json(players);
+    } else if (req.method === 'POST') {
+      const { name, image } = req.body;
+      const newPlayer = await prisma.player.create({
+        data: {
+          name,
+          image,
+          points: 0,
+        },
+      });
+      res.status(201).json(newPlayer);
+    } else {
+      res.setHeader('Allow', ['GET', 'POST']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+  } catch (error) {
+    console.error('Error fetching players:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 }
