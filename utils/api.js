2dcc99ffbda54143bddbec1249362f2526937e9b
jonte200488 <107001212+jonte200488@users.noreply.github.com>
2024-08-11 07:20:03 +0000

    3

--- utils/api.js
+++ utils/api.js
@@ -1,20 +1,28 @@
 // /utils/api.js
 
-const API_BASE_URL = "http://localhost:3000"; // Update this with your production server URL if deployed
+// /utils/api.js
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000';
 
 export const fetchPlayers = async () => {
-  const response = await fetch(`${API_BASE_URL}/players`);
-  return await response.json();
-};
+  try {
+    const response = await fetch(`${API_BASE_URL}/players`);
 
-export const addPlayer = async (name, image) => {
-  await fetch(`${API_BASE_URL}/players`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ name, image, points: 0 }),
-  });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const contentType = response.headers.get('Content-Type');
+
+    if (contentType && contentType.includes('application/json')) {
+      return await response.json(); // Parse JSON if content type is application/json
+    } else {
+      throw new Error('Unexpected content type: Expected JSON');
+    }
+  } catch (error) {
+    console.error('Failed to fetch players:', error.message);
+    throw error;
+  }
 };
 
 export const removePlayer = async (playerId) => {
