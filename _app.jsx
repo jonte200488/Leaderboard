b9b259422dc9ede65fcddfb45844802905b14c3f
jonte200488 <107001212+jonte200488@users.noreply.github.com>
2024-08-12 19:59:09 +0000

    Adding app.jsx

---
+++ _app.jsx
@@ -0,0 +1,8 @@
+// pages/_app.jsx
+import '../styles/style.css'; // Import your global CSS file here
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
