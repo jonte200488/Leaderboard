05299661a3de348c8518d297a67c524708887df3
jonte200488 <107001212+jonte200488@users.noreply.github.com>
2024-08-12 12:15:06 +0000

    leaderboard sorting

--- components/Leaderboard.js
+++ components/Leaderboard.js
@@ -12,7 +12,9 @@ export default function Leaderboard() {
     const response = await fetch('/api/players');
     const data = await response.json();
 
-    // Ensure data is sorted by totalPoints (although it should be sorted by the API)
+    // Ensure the players are sorted by totalPoints in descending order
+    data.sort((a, b) => b.totalPoints - a.totalPoints);
+
     setPlayers(data);
   };
 
@@ -20,13 +22,25 @@ export default function Leaderboard() {
     <div>
       <h2>Leaderboard</h2>
       <section id="podium" className={styles.podium}>
-        {players.slice(0, 3).map((player, index) => (
-          <div key={player.id} className={`${styles.podiumPosition} ${styles[`podium-${index + 1}`]}`}>
-            <img src={player.image} alt={player.name} className={styles.podiumImage} />
-            <div className={styles.playerName}>{player.name}</div>
-            <div className={styles.playerPoints}>{player.totalPoints} Points</div>
-          </div>
-        ))}
+        {players.length >= 3 && (
+          <>
+            <div key={players[1].id} className={`${styles.podiumPosition} ${styles['podium-2']}`}>
+              <img src={players[1].image} alt={players[1].name} className={styles.podiumImage} />
+              <div className={styles.playerName}>{players[1].name}</div>
+              <div className={styles.playerPoints}>{players[1].totalPoints} Points</div>
+            </div>
+            <div key={players[0].id} className={`${styles.podiumPosition} ${styles['podium-1']}`}>
+              <img src={players[0].image} alt={players[0].name} className={styles.podiumImage} />
+              <div className={styles.playerName}>{players[0].name}</div>
+              <div className={styles.playerPoints}>{players[0].totalPoints} Points</div>
+            </div>
+            <div key={players[2].id} className={`${styles.podiumPosition} ${styles['podium-3']}`}>
+              <img src={players[2].image} alt={players[2].name} className={styles.podiumImage} />
+              <div className={styles.playerName}>{players[2].name}</div>
+              <div className={styles.playerPoints}>{players[2].totalPoints} Points</div>
+            </div>
+          </>
+        )}
       </section>
       <section id="otherPlayers" className={styles.otherPlayers}>
         {players.slice(3).map((player) => (
