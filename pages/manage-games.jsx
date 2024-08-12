c59602dd2e383823e5a8b6395900371587d5c5da
jonte200488 <107001212+jonte200488@users.noreply.github.com>
2024-08-12 11:32:44 +0000

    dsffdg

--- pages/manage-games.jsx
+++ pages/manage-games.jsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css';
+import Navbar from '../components/Navbar';
 
 export default function Games() {
   const [games, setGames] = useState([]);
