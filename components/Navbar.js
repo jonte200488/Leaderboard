9c8076034139e32f9ae2ff7691fc522a009ddcfb
jonte200488 <107001212+jonte200488@users.noreply.github.com>
2024-08-13 11:42:14 +0000

    Navbar active style

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,20 @@
-// /components/Navbar.js
-
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Navbar() {
+  const router = useRouter();
+
   return (
     <nav className="navbar">
-      <Link href="/">Leaderboard</Link>
-      <Link href="/manage-games">Manage Games</Link>
-      <Link href="/manage-players">Manage Players</Link>
+      <Link href="/" passHref>
+        <a className={router.pathname === '/' ? 'active' : ''}>Leaderboard</a>
+      </Link>
+      <Link href="/manage-games" passHref>
+        <a className={router.pathname === '/manage-games' ? 'active' : ''}>Manage Games</a>
+      </Link>
+      <Link href="/manage-players" passHref>
+        <a className={router.pathname === '/manage-players' ? 'active' : ''}>Manage Players</a>
+      </Link>
     </nav>
   );
 }
