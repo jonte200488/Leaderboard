666c25b7e0cf3b7cf254874108bef6b3545c6914
jonte200488 <107001212+jonte200488@users.noreply.github.com>
2024-09-05 10:53:14 +0000

    Testing

--- components/Leaderboard.js
+++ components/Leaderboard.js
@@ -36,16 +36,24 @@ export default function Leaderboard() {
               </Link>
             </div>
             <div key={players[0].id} className="podiumPosition podium-1">
-              <img src={players[0].image} alt={players[0].name} className="podiumImage" />
-              <div className="playerName">{players[0].name}</div>
-              <div className="playerPoints">{players[0].averageWins.toFixed(0)}%</div>
-              <div className="playerName">{players[0].totalPoints}</div>
+              <Link href={`/player/${players[0].id}`}>
+                <a>
+                  <img src={players[0].image} alt={players[0].name} className="podiumImage" />
+                  <div className="playerName">{players[0].name}</div>
+                  <div className="playerPoints">{players[0].averageWins.toFixed(0)}%</div>
+                  <div className="playerName">{players[0].totalPoints}</div>
+                </a>
+              </Link>
             </div>
             <div key={players[2].id} className="podiumPosition podium-3">
-              <img src={players[2].image} alt={players[2].name} className="podiumImage" />
-              <div className="playerName">{players[2].name}</div>
-              <div className="playerPoints">{players[2].averageWins.toFixed(0)}%</div>
-              <div className="playerName">{players[2].totalPoints}</div>
+              <Link href={`/player/${players[2].id}`}>
+                <a>
+                  <img src={players[2].image} alt={players[2].name} className="podiumImage" />
+                  <div className="playerName">{players[2].name}</div>
+                  <div className="playerPoints">{players[2].averageWins.toFixed(0)}%</div>
+                  <div className="playerName">{players[2].totalPoints}</div>
+                </a>
+              </Link>
             </div>
           </>
         )}
@@ -53,12 +61,18 @@ export default function Leaderboard() {
       <section id="otherPlayers" className="otherPlayers">
         {players.slice(3).map((player) => (
           <div key={player.id} className="playerEntry">
-            <div className="playerInfo">
-              <img src={player.image} alt={player.name} className="playerImage" />
-              <span className="playerName">{player.name}</span>
-            </div>
-            <span className="playerPoints">{player.averageWins.toFixed(0)}%</span>
-            <span className="playerName">{player.totalPoints}</span>
+            <Link href={`/player/${player.id}`}>
+              <a>
+                <div className="playerInfo">
+                  <img src={player.image} alt={player.name} className="playerImage" />
+                  <span className="playerName">{player.name}</span>
+                </div>
+                <div>
+                  <span className="playerPoints">{player.averageWins.toFixed(0)}% </span>
+                  <span className="playerName">{player.totalPoints}</span>
+                </div>
+              </a>
+            </Link>
           </div>
         ))}
       </section>
