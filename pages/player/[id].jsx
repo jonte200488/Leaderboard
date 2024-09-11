842c68bad9b57f66a7fae8952d750222d53b434a
jonte200488 <107001212+jonte200488@users.noreply.github.com>
2024-09-11 14:55:13 +0000

    ghvjgkkj

--- pages/player/[id].jsx
+++ pages/player/[id].jsx
@@ -27,7 +27,7 @@ export default function PlayerPage() {
       const data = await response.json(); // Parse the JSON response
 
       // Use .find() to get the player with the matching id
-      const player = data.find((player) => player.id === id);
+      player = data.find((id) => player.id === id);
 
       if (player) {
         setPlayer(player); // Set the player's data
