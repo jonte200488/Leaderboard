955a879078feb9254e654da730517974d76166de
jonte200488 <107001212+jonte200488@users.noreply.github.com>
2024-09-14 21:56:28 +0000

    flödl

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,8 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
 
 export default function Navbar() {
-  const router = useRouter();
 
   return (
-    <nav className="navbar">
-      <Link href="/" passHref>
-        <a className={router.pathname === '/' ? 'active' : ''}>Leaderboard</a>
-      </Link>
-      <Link href="/manage-games" passHref>
-        <a className={router.pathname === '/manage-games' ? 'active' : ''}>Manage Games</a>
-      </Link>
-      <Link href="/manage-players" passHref>
-        <a className={router.pathname === '/manage-players' ? 'active' : ''}>Manage Players</a>
-      </Link>
-    </nav>
+    <div className='topBar'>
+    </div>
   );
 }
